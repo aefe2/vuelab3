930f0b44b5eeee8b38a44082174a712e8e046fa9
gr413_shiivi <shigaev_1990@mail.ru>
2023-03-06 11:29:39 +0700

    base from lab2

--- js/main.js
+++ js/main.js
@@ -3,63 +3,61 @@ let eventBus = new Vue();
 Vue.component('container', {
     data() {
         return {
-            columns: [[], [], [], []],
-            isEdit: false,
+            firstCol: [],
+            secondCol: [],
+            thirdCol: [],
         }
     },
     methods: {
-        // save() {
-        //     localStorage.firstCol = JSON.stringify(this.firstCol)
-        //     localStorage.secondCol = JSON.stringify(this.secondCol)
-        //     localStorage.thirdCol = JSON.stringify(this.thirdCol)
-        // },
+        save() {
+            localStorage.firstCol = JSON.stringify(this.firstCol)
+            localStorage.secondCol = JSON.stringify(this.secondCol)
+            localStorage.thirdCol = JSON.stringify(this.thirdCol)
+        },
+        time(idNote) {
+            let timeData = new Date();
+            this.secondCol[idNote].time = timeData.getHours() + ':' + timeData.getMinutes();
+            this.secondCol[idNote].date = timeData.getDate() + '.' + timeData.getMonth() + '.' + timeData.getFullYear();
+        },
     },
-    //card-note
     mounted() {
-        eventBus.$on('note-submitted', createdNote => {
-            this.columns[0].push(createdNote);
+        if (localStorage.firstCol) {
+            this.firstCol = JSON.parse(localStorage.firstCol)
+        }
+        if (localStorage.secondCol) {
+            this.secondCol = JSON.parse(localStorage.secondCol)
+        }
+        if (localStorage.thirdCol) {
+            this.thirdCol = JSON.parse(localStorage.thirdCol)
+        }
+        eventBus.$on('move-column2', (idNote, note) => {
+            if (this.secondCol.length < 5) {
+                if (this.firstCol[idNote].doneNum >= 50) {
+                    this.secondCol.push(this.firstCol[idNote])
+                    this.firstCol.splice(idNote, 1)
+                    this.save()
+                }
+            }
+            // } else if (this.secondCol.length === 5) {
+            //
+            // }
         });
-        eventBus.$on('delete-note', (idCol, idNote) => {
-            this.columns[idCol].splice(idNote, 1)
-        });
-        // if (localStorage.firstCol) {
-        //     this.firstCol = JSON.parse(localStorage.firstCol)
-        // }
-        // if (localStorage.secondCol) {
-        //     this.secondCol = JSON.parse(localStorage.secondCol)
-        // }
-        // if (localStorage.thirdCol) {
-        //     this.thirdCol = JSON.parse(localStorage.thirdCol)
-        // }
-        // eventBus.$on('move-column2', (idNote, note) => {
-        //     if (this.secondCol.length < 5) {
-        //         if (this.firstCol[idNote].doneNum >= 50) {
-        //             this.secondCol.push(this.firstCol[idNote])
-        //             this.firstCol.splice(idNote, 1)
-        //             this.save()
-        //         }
-        //     }
-        // } else if (this.secondCol.length === 5) {
-        //
-        // }
-        // });
-        // eventBus.$on('move-column3', (idNote, note) => {
-        //     if (this.secondCol[idNote].doneNum === 100) {
-        //         this.time(idNote)
-        //         this.thirdCol.push(this.secondCol[idNote])
-        //         this.secondCol.splice(idNote, 1)
-        //         this.save()
-        //     }
-        // })
+        eventBus.$on('move-column3', (idNote, note) => {
+            if (this.secondCol[idNote].doneNum === 100) {
+                this.time(idNote)
+                this.thirdCol.push(this.secondCol[idNote])
+                this.secondCol.splice(idNote, 1)
+                this.save()
+            }
+        })
     },
     template: `
     <div>
         <create-form></create-form>
         <div class="container">
-            <column1 class="column column1" :firstCol="firstCol"></column1>
+            <column1 :class="{ disabled: secondCol.length === 5 }" class="column column1" :firstCol="firstCol"></column1>
             <column2 class="column column2" :secondCol="secondCol"></column2>
             <column3 class="column column3" :thirdCol="thirdCol"></column3>
-            <column4 class="column column4" :fourthCol="fourthCol"></column4>
         </div>
     </div>
     `,
@@ -71,28 +69,25 @@ Vue.component('column1', {
             type: Array,
             required: true
         },
-        idCol: {
-            type: Number
-        }
     },
     data() {
         return {}
     },
     mounted() {
         eventBus.$on('on-submit', createNote => {
-            this.firstCol.push(createNote)
-            // this.save()
-            // }
+            if (this.firstCol.length < 3) {
+                this.firstCol.push(createNote)
+                this.save()
+            }
         })
     },
     methods: {
-        // save() {
-        //     localStorage.firstCol = JSON.stringify(this.firstCol)
-        // },
+        save() {
+            localStorage.firstCol = JSON.stringify(this.firstCol)
+        },
     },
     template: `
      <div>
-        <h2>Scheduled tasks</h2>
         <note v-for="(note, index) in firstCol" @save="save()" :firstCol="firstCol" :key="note.key" :idNote="index" :note="note">
             
         </note>
@@ -111,13 +106,12 @@ Vue.component('column2', {
         return {}
     },
     methods: {
-        // save() {
-        //     localStorage.secondCol = JSON.stringify(this.secondCol)
-        // }
+        save() {
+            localStorage.secondCol = JSON.stringify(this.secondCol)
+        }
     },
     template: `
      <div>
-        <h2>Tasks at work</h2>
         <note v-for="(note, index) in secondCol" @save="save()" :secondCol="secondCol" :key="note.key" :idNote="index" :note="note">
             
         </note>
@@ -136,39 +130,13 @@ Vue.component('column3', {
         return {}
     },
     methods: {
-        // save() {
-        //     localStorage.thirdCol = JSON.stringify(this.thirdCol)
-        // }
-    },
-    template: `
-     <div>
-        <h2>Testing</h2>
-        <note v-for="(note, index) in thirdCol" @save="save()" :thirdCol="thirdCol" :key="note.key" :idNote="index" :note="note">
-            
-        </note>
-    </div>
-    `,
-})
-
-Vue.component('column4', {
-    props: {
-        fourthCol: {
-            type: Array,
-            required: true
+        save() {
+            localStorage.thirdCol = JSON.stringify(this.thirdCol)
         }
     },
-    data() {
-        return {}
-    },
-    methods: {
-        // save() {
-        //     localStorage.thirdCol = JSON.stringify(this.thirdCol)
-        // }
-    },
     template: `
      <div>
-        <h2>Completed tasks</h2>
-        <note v-for="(note, index) in fourthCol" @save="save()" :fourthCol="fourthCol" :key="note.key" :idNote="index" :note="note">
+        <note v-for="(note, index) in thirdCol" @save="save()" :thirdCol="thirdCol" :key="note.key" :idNote="index" :note="note">
             
         </note>
     </div>
@@ -183,9 +151,6 @@ Vue.component('note', {
         idNote: {
             type: Number,
         },
-        idCol: {
-            type: Number
-        }
     },
     data() {
         return {
@@ -195,108 +160,124 @@ Vue.component('note', {
         }
     },
     methods: {
-        deleteNote(idNote, idCol) {
-            eventBus.$emit('delete-note', idNote, idCol)
+        addTask() {
+            if (this.taskTitle && this.note.tasks.length < 5) {
+                let createTask = {
+                    taskTitle: this.taskTitle,
+                    isDone: false
+                }
+                this.note.tasks.push(createTask);
+                this.taskTitle = '';
+                this.$emit('save')
+            }
         },
-        editNote(idNote, idCol) {
-            eventBus.$emit('edit-note', idNote, idCol)
-        }
     },
     mounted() {
-
+        eventBus.$on('update-checkbox', idNote => {
+            let doneCount = 0;
+            let notDoneCount = 0;
+            let allTasksCount = 0;
+            for (let task of this.note.tasks) {
+                allTasksCount++;
+                if (task.isDone === true) {
+                    doneCount++;
+                } else {
+                    notDoneCount++;
+                }
+            }
+            this.note.doneNum = (doneCount / (doneCount + notDoneCount)) * 100;
+            if (this.note.doneNum > 50) eventBus.$emit('move-column2', idNote, this.note);
+            if (this.note.doneNum === 100) eventBus.$emit('move-column3', idNote, this.note);
+        })
     },
     template: `
     <div class="todo-card todo-item">
         <div class="todo-title">
             <span>{{ note.title }}</span>
         </div>
-        <div>
-            <span>{{ note.taskTitle }}</span>
-        </div>
+        <task v-for="(task, key) in note.tasks" :key="key" :task="task" :idNote="idNote"></task>
+        <form class="add-task-form" v-show="this.note.tasks.length < 5 && this.note.doneNum !== 100" @submit.prevent="addTask">
+            <input class="task-title-input" placeholder="new task" v-model="taskTitle" type="text">
+            <input class="submit-btn" type="submit" value="+">
+        </form>
         <div class="date" v-if="note.date">
             <span>Date - {{ note.date }}</span>
             <span>Time - {{ note.time }}</span>
         </div>
-        <div class="deadline">
-            <span>Date - {{ note.deadlineDate }}</span>
-            <span>Time - {{ note.deadlineTime }}</span>
-        </div>
-        <div class="delete-block">
-            <button @click="deleteNote(idNote)">Delete</button>
-        </div>
     </div>`,
 })
 
-Vue.component('edit-note', {
-    props: {},
+Vue.component('task', {
+    props: {
+        task: {
+            type: Object
+        },
+        idNote: {
+            type: Number,
+        },
+    },
     data() {
-        return {
-            title: null,
-            taskTitle: null,
-            deadlineDate: null,
-            deadlineTime: null,
-            editTime: null,
-            editDate: null
-        }
+        return {}
     },
     methods: {
-        onSubmit() {
-            if (this.title || this.taskTitle || this.deadlineTime || this.deadlineDate) {
-                let DateTime = new Date()
-                let editNote = {
-                    title: this.title,
-                    taskTitle: this.taskTitle,
-                    deadlineDate: this.deadlineDate,
-                    deadlineTime: this.deadlineTime,
-                    editDate: DateTime.getFullYear() + '-' + DateTime.getMonth() + '-' + DateTime.getDay(),
-                    editTime: DateTime.getHours() + ':' + DateTime.getMinutes()
-                }
-                this.$emit('edit-submitted', editNote)
-            } else {
-                let editNote = {}
-                this.$emit('edit-submitted', editNote)
-            }
-            this.title = null;
-            this.taskTitle = null;
-            this.deadlineDate = null;
-            this.deadlineTime = null;
+        updateCounter() {
+            this.task.isDone = !this.task.isDone
+            eventBus.$emit('update-checkbox', this.idNote)
         }
     },
     mounted() {
+
     },
     template: `
-<form @onsubmit="">
-    
-</form>
-    `,
+    <div class="task">
+        <span class="task-title">{{ task.taskTitle }}</span>
+        <button :class="{done: task.isDone}" 
+        class="done-btn"
+        :disabled="task.isDone"
+        @click="updateCounter()">Done</button>
+<!--        <button v-show="task.isDone" -->
+<!--        class="undone-btn"-->
+<!--        @click="updateCounter()">Undone</button>-->
+    </div>`,
 })
 
 Vue.component('create-form', {
     data() {
         return {
             title: null,
-            taskTitle: null,
-            date: null,
-            time: null,
-            deadlineDate: null,
-            deadlineTime: null
+            taskTitle1: null,
+            taskTitle2: null,
+            taskTitle3: null,
+            isDone: null,
+            status: 0,
         };
     },
     methods: {
         onSubmit() {
-            if (this.title && this.taskTitle) {
-                let timeData = new Date();
+            if (this.title && this.taskTitle1 && this.taskTitle2 && this.taskTitle3) {
                 let createNote = {
                     title: this.title,
-                    taskTitle: this.taskTitle,
-                    time: timeData.getHours() + ':' + timeData.getMinutes(),
-                    date: timeData.getFullYear() + '-' + Number(timeData.getMonth() + 1) + '-' + timeData.getDate(),
-                    deadlineDate: this.deadlineDate,
-                    deadlineTime: this.deadlineTime
+                    tasks: [
+                        {
+                            taskTitle: this.taskTitle1,
+                            isDone: false
+                        },
+                        {
+                            taskTitle: this.taskTitle2,
+                            isDone: false
+                        },
+                        {
+                            taskTitle: this.taskTitle3,
+                            isDone: false
+                        },
+                    ],
+                    status: 1,
                 }
                 eventBus.$emit('on-submit', createNote);
                 this.title = '';
-                this.taskTitle = '';
+                this.taskTitle1 = '';
+                this.taskTitle2 = '';
+                this.taskTitle3 = '';
             }
         },
     },
@@ -304,12 +285,9 @@ Vue.component('create-form', {
     <form class="create-form" @submit.prevent="onSubmit">
         <label>Create Todo</label>
         <input v-model="title" type="text" placeholder="title">
-        <input v-model="taskTitle" type="text" placeholder="description">
-        <span>Deadline</span>
-        <div class="date-input">
-            <input type="date" v-model="deadlineDate">
-            <input type="time" v-model="deadlineTime">
-        </div>
+        <input v-model="taskTitle1" type="text" placeholder="task - 1">
+        <input v-model="taskTitle2" type="text" placeholder="task - 2">
+        <input v-model="taskTitle3" type="text" placeholder="task - 3">
         <input type="submit" value="Create">
     </form>
     `,
